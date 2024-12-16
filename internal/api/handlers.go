9657fe1614ff55bd56b182050d372e4a1d386209
Kristián Řehoř <kmancz.kiki@gmail.com>
2024-12-16 18:44:32 +0100

    basic template handeling

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,10 +1,22 @@
 package api
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 )
 
 func handleLandingPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello to fajntvajb")
+	//TODO: move up so it can be open only once
+	files := os.DirFS(".")
+	tmpl, err := template.New("").ParseFS(files, "templates/*")
+	if err != nil {
+		http.Error(w, "Something went wrong", 500)
+	}
+
+	tmpl.ExecuteTemplate(w, "index.html", struct {
+		Name string
+	}{
+		Name: "John Doe",
+	})
 }
