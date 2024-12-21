c80db9d611350923a90f6bb2b534f11e7336b1e6
Kristián Řehoř <kmancz.kiki@gmail.com>
2024-12-21 15:08:14 +0100

    Added port env var and right log type

--- cmd/fajtvajb/main.go
+++ cmd/fajtvajb/main.go
@@ -4,25 +4,32 @@ import (
 	"fajntvajb/internal/api"
 	"fajntvajb/internal/logger"
 	"net/http"
+	"os"
 )
 
 func main() {
 	log := logger.Get()
 	router, err := api.New()
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to create router")
+		log.Fatal().Err(err).Msg("Failed to create router")
 		return
 	}
 
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: logger.RequestLogger(router),
 	}
 
-	log.Info().Msg("Listening on port 8080")
-	log.Info().Msg("Server can be accesed on http://localhost:8080")
+	log.Info().Str("port", port).Msg("Listening on port " + port)
+	log.Info().Msg("Server can be accesed on http://localhost:" + port)
+
 	err = server.ListenAndServe()
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to start server")
+		log.Fatal().Err(err).Msg("Failed to start server")
 	}
 }
