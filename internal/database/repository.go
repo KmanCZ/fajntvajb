5d2cba659324cb66c6c3f233b626320f5f7a7de7
Kristián Řehoř <kmancz.kiki@gmail.com>
2024-12-22 16:16:38 +0100

    Created users table

--- internal/database/repository.go
+++ internal/database/repository.go
@@ -1,1 +1,30 @@
 package database
+
+type User struct {
+	ID          int
+	Username    string
+	DisplayName string
+	Password    string
+}
+
+func (db *DB) GetUserByUsername(username string) (*User, error) {
+	user := &User{}
+	err := db.connection.Get(user, "SELECT * FROM users WHERE username = $1", username)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (db *DB) CreateUser(username, displayName, password string) (*User, error) {
+	user := &User{
+		Username:    username,
+		DisplayName: displayName,
+		Password:    password,
+	}
+	_, err := db.connection.NamedExec("INSERT INTO users (username, display_name, password) VALUES (:username, :display_name, :password)", user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
